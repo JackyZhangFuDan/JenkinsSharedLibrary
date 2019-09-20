19a91f38a7f677dbac24cfd874d8b86e6dc98961
Jacky <jacky01.zhang@outlook.com>
2019-09-20 14:57:52 +0800

    add function to download from jenkins

--- src/com/sap/calm/ptm/jenkinslibrary/DownloadUtil.groovy
+++ src/com/sap/calm/ptm/jenkinslibrary/DownloadUtil.groovy
@@ -1,17 +1,168 @@
 package com.sap.calm.ptm.jenkinslibrary
 
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import java.text.SimpleDateFormat
+import java.nio.charset.StandardCharsets
+
+//@Grab('commons-io:commons-io:2.5')
+import org.apache.commons.io.IOUtils
+import org.apache.http.HttpEntity
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.conn.ssl.TrustStrategy
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.ssl.SSLContexts
+import org.apache.http.util.EntityUtils
+
+import groovy.json.JsonSlurper
+
 class DownloadUtil{
-	String url = null
-	String folder = null
 	
-	def DownloadUtil(String url,String folder){
-		this.url = url
-		this.folder = folder
-		println('specified url:'+this.url)
-		println('specified target folder:'+this.folder)
+	def DownloadUtil(){
 	}
 	
-	def boolean download(){
+	/**
+	 * Downlaod test results of the specified job from another jenkins server, 
+	 * it only considers the result created in the specified date
+	 * @return
+	 */
+	def boolean downloadFromJenkins(String jenkinsServer, String jobName, Date date, String folderOfCurrentWorkspace, String relativePathOfToBeDownloadedFile){
+		SimpleDateFormat sdf = new SimpleDateFormat('yyyyMMdd')
+		String targetDateStr = sdf.format(date)
+		
+		File targetFolder = new File(folderOfCurrentWorkspace);
+		if(!targetFolder.exists() || !targetFolder.isDirectory()){
+			println 'The specified target folder does not exist.'
+			return false;
+		}else{
+			FileTreeBuilder targetFolderBuilder = new FileTreeBuilder(targetFolder)
+			targetFolder = targetFolderBuilder.dir("downloadedTestResult")
+		}
+		
+		String url = jenkinsServer + '/job/' + jobName + '/api/json'
+		String s = this.HttpsGetWithoutCert(url)
+		def jsonSlurper = new JsonSlurper()
+		def jobJsonObject = jsonSlurper.parseText(s);
+		ArrayList jobBuilds = jobJsonObject.builds;
+		ArrayList toBeDownloadedBuilds = new ArrayList()
+		
+		//which builds to download?
+		jobBuilds.each( { build ->
+			try{
+				s = this.HttpsGetWithoutCert(build.url + "/api/json")
+				def buildJsonObject = jsonSlurper.parseText(s)
+				def ts = new Date(buildJsonObject.timestamp)
+				if(sdf.format(ts).equals( targetDateStr) ){
+					toBeDownloadedBuilds.add(build);
+				}
+			}catch(Exception ex){
+				
+			}
+		})
+		
+		//download the builds
+		toBeDownloadedBuilds.each({ build ->
+			url = build.url + relativePathOfToBeDownloadedFile
+			this.download(url, targetFolder,jobName+'_'+build.number+'_resultfile')
+		})
+		
 		return true
+	}
+	
+	private def boolean download(String url, File targetFolder, String fileName){
+		
+		File targetFile = new File(targetFolder, fileName);
+		
+		String s = ""
+		try{
+			s = this.HttpsGetWithoutCert(url)
+		}catch(Exception ex){
+			ex.printStackTrace()
+			return false
+		}
+		
+		targetFile.withWriter('utf-8'){ writer ->
+			writer.write(s)
+		};
+		
+		return true;
+		
+		/*
+		//download the file
+		def url = new URL(this.url)
+		def connection = null
+		try{
+			connection = url.openConnection()
+			connection.requestMethod = 'GET'
+		}catch(Exception ex){
+			println 'Download fail, there is exception when opening the url, '+ex.getMessage()
+			return false;
+		}
+		if (connection.responseCode == 200) {
+			String s = "";
+			connection.getInputStream().withReader('utf-8'){reader ->
+				s = reader.getText();
+				new File(this.folder, this.fileName).withWriter('utf-8'){ writer ->
+					writer.write(s)
+				};
+			}
+			
+			//print some helpful information
+			//println connection.content.text
+			//println connection.contentEncoding
+			println 'type of the downloaded content: ' + connection.contentType
+			println 'last modify timestamp of the content: ' + connection.lastModified
+			println 'HTTP header of the response: '
+			connection.headerFields.each { 
+				println "> ${it}"
+			}
+			
+		} else {
+			println 'download fail, the http reponse code isn''t 200: ' + connnection.responseCode
+			return false
+		}
+		return true
+		*/
+	}
+	
+	private def String HttpsGetWithoutCert(String url) throws Exception {
+		SSLContext sslcontext = SSLContexts.custom()
+				.loadTrustMaterial(new TrustStrategy() {
+					//ignore checking server's certification
+					public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+						return true;
+					}
+				})
+				.build()
+		
+		String[] ps = ["TLSv1","TLSv1.1","TLSv1.2"]
+		SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
+				sslcontext,
+				ps,
+				null,
+				SSLConnectionSocketFactory.getDefaultHostnameVerifier())
+		
+		CloseableHttpClient httpclient = HttpClients.custom()
+				.setSSLSocketFactory(sslConnectionSocketFactory)
+				.build()
+ 
+		HttpGet httpget = new HttpGet(url)
+		
+		System.out.println("Executing request " + httpget.getRequestLine())
+		CloseableHttpResponse response = httpclient.execute(httpget)
+		try {
+			HttpEntity entity = response.getEntity()
+			System.out.println(response.getStatusLine())
+			String result = IOUtils.toString(entity.getContent())
+			return result
+			//EntityUtils.consume(entity)
+		}catch(Exception ex){
+			response.close()
+			throw ex
+		}
 	}
 }
--- test/groovy/exampleJob.groovy
+++ test/groovy/exampleJob.groovy
@@ -10,13 +10,19 @@ def execute() {
             echo "Something"
         }
         stage("Middle") {
-            DownloadUtil util = new DownloadUtil('https://jaas.wdf.sap.corp:30132/job/PTM_API_Test_Q/api$ptm_api_test_quality/585/testReport/Quality_PM_API_Test/Task/Update_task_N/','c:/jacky')
-            println util.download()
+            DownloadUtil util = new DownloadUtil()
+            println util.downloadFromJenkins('https://gkecalmdevshanghai.jaas-gcp.cloud.sap.corp', 'PTM_CN_FRAME_UI',new Date(1568611787631),pwd(),'artifact/target/frame/UT/coverage/IE%2011.0.0%20(Windows%2010.0.0)/node_modules/karma-qunit/lib/index.html')
         }
+		
         stage("Last") {
             echo "We're done"
         }
     }
+	
+}
+
+def pwd(){
+	return 'C:\\Users\\i042102\\Downloads'
 }
 
 return this
