24c99a67b4a70404aae0b309d2c0deb04f5d08e5
Jacky <jacky01.zhang@outlook.com>
2019-09-22 22:10:41 +0800

    add pds notification

---
+++ src/com/sap/calm/ptm/jenkinslibrary/PDSNotificationUtil.groovy
@@ -0,0 +1,30 @@
+package com.sap.calm.ptm.jenkinslibrary
+
+import com.sap.calm.ptm.jenkinslibrary.PDSNotificationBuilder
+import static com.sap.calm.ptm.jenkinslibrary.PDSNotificationBuilder.PDSNotification
+
+class PDSNotificationUtil {
+	public static boolean notifyPDS(
+		String project, String module, String server, String serverType, String category, 
+		String jobName, String buildId, Date whenTestRun, List files){
+		
+		PDSNotificationBuilder notifBuilder = new PDSNotificationBuilder(
+			project, 
+			module, 
+			server, 
+			serverType, 
+			category
+		)
+		notifBuilder.jobName(jobName).jobBuildId(buildId).whenTestRun(whenTestRun)
+		
+		if(files != null && !files.empty){
+			for(int i = 0; i < files.size(); i++){
+				println files.get(i)
+				notifBuilder.addFile(files.get(i).format,files.get(i).url);
+			}
+		}
+		
+		PDSNotification notification = notifBuilder.build()
+		return notification.send()
+	}
+}
--- test/groovy/exampleJob.groovy
+++ test/groovy/exampleJob.groovy
@@ -3,16 +3,20 @@ import com.lesfurets.jenkins.unit.global.lib.Library
 @Library('pdsjenkinslibrary@master') _
 
 import com.sap.calm.ptm.jenkinslibrary.DownloadUtil
+import com.sap.calm.ptm.jenkinslibrary.PDSNotificationUtil
 
 def execute() {
     node() {
+		DownloadUtil downloadUtil
         stage("First") {
             echo "Something"
+            downloadUtil = new DownloadUtil()
         }
         stage("Middle") {
-            DownloadUtil util = new DownloadUtil()
             
-			List<String> result = util.downloadFromJenkins(
+			def dateToBeDownloaded = new Date() - 1
+			
+			List<String> result = downloadUtil.downloadFromJenkins(
 				'https://gkecalmdevshanghai.jaas-gcp.cloud.sap.corp', //server of the jenkins
 				'PTM_CN_FRAME_UI',                                    //job name
 				new Date(1568611787631),                              //which date's builds of this job will be downloaded 
@@ -20,18 +24,39 @@ def execute() {
 				'artifact/target/frame/UT/coverage/IE%2011.0.0%20(Windows%2010.0.0)/node_modules/karma-qunit/lib/index.html')
 			
 			echo 'Downloaded files: '
+			
 			result.forEach({line ->
 				println line
 			})
 			
 			echo "Current workspace: ${WORKSPACE}"
         }
-		
-        stage("Post") {
-			echo 'Downloaded fileds are archived. ' + DownloadUtil.DOWNLOADSUBFOLDER
+		stage('Archive Files'){
 			archiveArtifacts artifacts: DownloadUtil.DOWNLOADSUBFOLDER + '/**'
+			echo 'Downloaded fileds are archived, they are putted to folder ' + DownloadUtil.DOWNLOADSUBFOLDER + ' which is subfolder of build"s archieve folder'
+	   }
+	   stage('Notify PDS'){
+			boolean notiResult = PDSNotificationUtil.notifyPDS(
+				'calmptm', 
+				'project', 
+				'https://gkecalmdevshanghai.jaas-gcp.cloud.sap.corp', 
+				'jenkins', 
+				'ut_coverage_backend',
+				'jacky_sl_ut',
+				'1',
+				new Date(),
+				[
+					[format:'jacoco', url:'http://www.google.com'],
+					[format:'jacoco', url:'http://www.baidu.com']
+				]
+			)
 			
-        }
+			if(notiResult){
+				echo 'Notification is sent'
+			}else{
+				echo 'Send Notification fail'
+			}
+	   }
     }
 	
 }
