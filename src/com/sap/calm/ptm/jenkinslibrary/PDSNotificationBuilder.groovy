5800f6a7c5ea5e7482c3705745bd8750c6891e6e
Jacky <jacky01.zhang@outlook.com>
2019-09-22 22:38:14 +0800

    add pds notification 4

--- src/com/sap/calm/ptm/jenkinslibrary/PDSNotificationBuilder.groovy
+++ src/com/sap/calm/ptm/jenkinslibrary/PDSNotificationBuilder.groovy
@@ -7,32 +7,21 @@ import java.text.SimpleDateFormat
 import java.nio.charset.StandardCharsets
 import com.cloudbees.groovy.cps.NonCPS
 
-@Grab('commons-io:commons-io:2.5')
 import org.apache.commons.io.IOUtils
 
-@Grab('org.apache.httpcomponents:httpclient:4.5.6')
 import org.apache.http.HttpEntity
-@Grab('org.apache.httpcomponents:httpclient:4.5.6')
 import org.apache.http.ssl.SSLContexts
-@Grab('org.apache.httpcomponents:httpclient:4.5.6')
 import org.apache.http.util.EntityUtils
 
 import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.impl.auth.BasicScheme
 
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.client.methods.CloseableHttpResponse
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.client.methods.HttpPost
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.conn.ssl.TrustStrategy
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.impl.client.CloseableHttpClient
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.impl.client.HttpClients
-@Grab('org.apache.httpcomponents:httpcore:4.4.10')
 import org.apache.http.entity.StringEntity
 
 import groovy.json.JsonSlurper
@@ -80,126 +69,6 @@ public class PDSNotificationBuilder{
 			return this.notification
 		}else{
 			return null;
-		}
-	}
-	
-	public static class PDSNotification{
-		public String project
-		public String module
-		public String server
-		public String type
-		
-		public String job
-		public String build
-		public String category
-		public List<ResultFile> files
-		
-		public String whenTestRun
-		
-		private PDSNotification(){
-			
-		}
-		
-		public boolean send(){
-			if(!this.validate()){
-				println 'validation before sending fail.'
-				return false
-			}
-			def jsonStr = JsonOutput.toJson([
-				project:this.project,
-				module:this.module,
-				server:this.server,
-				type:this.type,
-				job:this.job,
-				build:this.build,
-				category:this.category,
-				whenTestRun:this.whenTestRun,
-				files:this.files
-			])
-			println 'Json data to be sent to PDS: ' + jsonStr
-			
-			try{
-				this.httpsPostData('https://solmancf1-approuter-pds.cfapps.sap.hana.ondemand.com/pdd/api/v1/send', jsonStr)
-			}catch(Exception ex){
-				ex.printStackTrace()
-				return false
-			}
-			
-			return true
-		}
-		
-		private boolean validate(){
-			if(this.project == null || this.project.isEmpty()){
-				return false;
-			}
-			if(this.module == null || this.module.isEmpty()){
-				return false;
-			}
-			if(this.server == null || this.server.isEmpty()){
-				return false;
-			}
-			if(this.type == null || this.type.isEmpty()){
-				return false;
-			}
-			if(this.category == null || this.category.isEmpty()){
-				return false;
-			}
-			return true
-		}
-		
-		@NonCPS
-		private def boolean httpsPostData(String url,String jsonStr) throws Exception {
-			
-			SSLContext sslcontext = SSLContexts.custom()
-				.loadTrustMaterial(new TrustStrategy() {
-					//ignore checking server's certification
-					@NonCPS
-					public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-						return true;
-					}
-				})
-				.build()
-			
-			String[] ps = ['TLSv1','TLSv1.1','TLSv1.2']
-			SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
-					sslcontext,
-					ps,
-					null,
-					SSLConnectionSocketFactory.getDefaultHostnameVerifier()
-			)
-			
-			CloseableHttpClient httpclient = HttpClients.custom()
-				.setSSLSocketFactory(sslConnectionSocketFactory)
-				.build()
-	  
-			HttpPost httppost = new HttpPost(url)
-			StringEntity entity = new StringEntity(jsonStr);
-		    httppost.setEntity(entity);
-		    httppost.setHeader("Accept", "application/json");
-		    httppost.setHeader("Content-type", "application/json");
-			
-			UsernamePasswordCredentials creds = new UsernamePasswordCredentials("JohnSnow", "Solman00")
-			httppost.addHeader(new BasicScheme().authenticate(creds, httppost, null));
-			
-			CloseableHttpResponse response = httpclient.execute(httppost)
-			
-			try {
-				println response.getStatusLine()
-				HttpEntity respEntity = response.getEntity()
-				//println respEntity.getContent()
-				
-				EntityUtils.consume(respEntity)
-			}catch(Exception ex){
-				throw ex
-			}
-			
-			int respCode = response.getStatusLine().getStatusCode();
-			if(respCode >= 200 && respCode < 300){
-				return true
-			}else{
-				return false
-			}
-			
 		}
 	}
 	
--- src/com/sap/calm/ptm/jenkinslibrary/PDSNotificationUtil.groovy
+++ src/com/sap/calm/ptm/jenkinslibrary/PDSNotificationUtil.groovy
@@ -1,7 +1,7 @@
 package com.sap.calm.ptm.jenkinslibrary
 
 import com.sap.calm.ptm.jenkinslibrary.PDSNotificationBuilder
-//import com.sap.calm.ptm.jenkinslibrary.PDSNotificationBuilder.PDSNotification
+import com.sap.calm.ptm.jenkinslibrary.PDSNotification
 
 class PDSNotificationUtil {
 	public static boolean notifyPDS(
@@ -24,7 +24,7 @@ class PDSNotificationUtil {
 			}
 		}
 		
-		PDSNotificationBuilder.PDSNotification notification = notifBuilder.build()
+		PDSNotification notification = notifBuilder.build()
 		return notification.send()
 	}
 }
