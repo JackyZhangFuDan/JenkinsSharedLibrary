aae145376b126d59da43587739f456bddf09d1bf
Jacky <jacky01.zhang@outlook.com>
2019-09-30 13:50:03 +0800

    update readme

--- src/com/sap/calm/ptm/jenkinslibrary/DownloadUtil.groovy
+++ src/com/sap/calm/ptm/jenkinslibrary/DownloadUtil.groovy
@@ -16,11 +16,11 @@ class DownloadUtil{
 	/**
 	 * Download test result from other jenkins server, this is static method which can be accessed from class, so it is easy for pipeline to comsume
 	 * 
-	 * @param jenkinsServer
-	 * @param jobName
-	 * @param date
-	 * @param folderOfCurrentWorkspace
-	 * @param relativePathOfToBeDownloadedFile
+	 * @param jenkinsServer: server of the target jenkins
+	 * @param jobName: name of the job which produced the target file
+	 * @param date: which date's data do you want to collect?
+	 * @param folderOfCurrentWorkspace: specify folder of current pipeline's workspace, the downloaded files will be putted here before archived
+	 * @param relativePathOfToBeDownloadedFile: the target file's relative path in remote jenkins server, you can ignore the server part of the full path to prepare this parameter 
 	 * @return: URI of the downloaded file
 	 */
 	public static List<String> downloadFromOtherJenkins(String jenkinsServer, String jobName, Date date, String folderOfCurrentWorkspace, String relativePathOfToBeDownloadedFile){
@@ -29,6 +29,17 @@ class DownloadUtil{
 		
 	}
 	
+	/**
+	 * This method will format data which can be used for sending PDS notification later for the collected files.
+	 * 
+	 * @param project: what is the project does these files belongs to
+	 * @param module: which module does these files belongs to
+	 * @param category: e.g. ut_coverage_backend......
+	 * @param fileFormat: e.g. ut_jacoco, eslint......
+	 * @param files: the collected files' name
+	 * @param urlPrefix: current jenkins' server + urlPrefix + file name = full path of the file in current jenkins server
+	 * @return
+	 */
 	public static Map prepareDataForNotification(String project, String module, String category, String fileFormat, List<String> files, String urlPrefix){
 		if(files == null || files.size() == 0) return null
 		
@@ -53,7 +64,7 @@ class DownloadUtil{
 	/**
 	 * Delete the specified folder and all its sub objects
 	 * 
-	 * @param folder
+	 * @param folder: 
 	 * @return
 	 */
 	public static boolean clearTmpFolder(String parent){
